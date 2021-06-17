6ffe24bd48e916d7790a7ba70de9ff403c27bac3
Ritik565 <54803142+Ritik565@users.noreply.github.com>
2021-06-17 11:51:19 +0530

    Create q3.js

---
+++ THA6/q3.js
@@ -0,0 +1,10 @@
+function first(inputArray, n = 1) {
+    return inputArray.slice(0,n);
+}
+
+console.log("\nQ3 solution.");
+console.log(first([7, 9, 0, -2]));
+console.log(first([],3));
+console.log(first([7, 9, 0, -2],3));
+console.log(first([7, 9, 0, -2],6));
+console.log(first([7, 9, 0, -2],-3));
