eeb9f6d63534247e9449383c2eca8b0d0fb46862
Ritik565 <54803142+Ritik565@users.noreply.github.com>
2021-07-17 21:19:58 +0530

    Create index.js

---
+++ THA18/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Board from "./board";
+import "./style.css";
+
+const App = () => {
+  return <Board />;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
+© 2021 GitHub, Inc.
