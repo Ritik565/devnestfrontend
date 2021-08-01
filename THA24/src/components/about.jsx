36149427f6177d49f0610fff5b8b8e6b846ba820
Ritik565 <54803142+Ritik565@users.noreply.github.com>
2021-08-01 20:09:31 +0530

    Create about.jsx

---
+++ THA24/src/components/about.jsx
@@ -0,0 +1,13 @@
+const About = ()=>{
+    return<div className="about page-container">
+        <div className="title">
+            ABOUT
+        </div>
+        <div className="content">
+            Public Route
+        </div>
+    </div>
+}
+
+export default About;
+© 2021 GitHub, Inc.
